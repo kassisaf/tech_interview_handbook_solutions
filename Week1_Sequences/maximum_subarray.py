38a752f1c4de246ddc1cbacaf5a4261232eb2717
ababyduck <kassisaf@gmail.com>
2022-05-05 18:50:47 -0700

    Add Maximum Subarray solution

--- Week1_Sequences/maximum_subarray.py
+++ Week1_Sequences/maximum_subarray.py
@@ -5,16 +5,34 @@ https://leetcode.com/problems/maximum-subarray/
 Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
 
 A subarray is a contiguous part of an array.
+
+Runtime: 733 ms, faster than 92.19% of Python3 online submissions for Maximum Subarray.
 """
+import sys
 from typing import List
 
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        pass
+        max_current = 0
+        max_global = -sys.maxsize
+
+        # Kadane's algorithm
+        for num in nums:
+            if max_current >= 0:
+                max_current += num
+            else:
+                max_current = num
+
+            if max_current > max_global:
+                max_global = max_current
+
+        return max_global
 
 
 if __name__ == "__main__":
     assert Solution.maxSubArray(None, nums=[-2,1,-3,4,-1,2,1,-5,4]) == 6
     assert Solution.maxSubArray(None, nums=[1]) == 1
     assert Solution.maxSubArray(None, nums=[5,4,-1,7,8]) == 23
+    assert Solution.maxSubArray(None, nums=[-1]) == -1
+    assert Solution.maxSubArray(None, nums=[-2,-1]) == -1
